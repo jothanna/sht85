0958a2aad0f9089bb1d18a5ff1a04576031fbf24
jothanna <51696780+jothanna@users.noreply.github.com>
2020-11-19 16:20:30 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sht85",
+    version="1.0",
+    packages=find_packages(),
+    description='Python driver for Sensirion SHT85 sensors connected to I2c pins of a Raspberrry Pi',
+    author = 'Johanna Redelstein',
+    license = 'GNU GPL',
+    url= 'https://github.com/jothanna/sht85/'
+)
